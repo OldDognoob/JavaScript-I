3a837efacd53ddf04e0c972584a3370e59e58e6c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-12-16 16:48:16 +0200

    correct function stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -36,13 +36,15 @@
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
 
-const triple = (arr) => arr.map(function(num) {return num * 3});
-console.log(triple([1,2,3,4]));
+// const triple = (arr) => arr.map(function(num) {return num * 3});
+// console.log(triple([1,2,3,4]));
 
+const triple = exampleArray.map((num)=> num *3);
+console.log(triple);
  
